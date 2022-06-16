78fef1079388f3a617ac13a585cb47fbffb6112b
jeff90813 <jeff90813@gmail.com>
2022-06-16 15:31:36 +0800

    Select number to csv & display to Demo HTML

--- html/miinv/js/load_csv.js
+++ html/miinv/js/load_csv.js
@@ -19,34 +19,41 @@ $(document).ready(function () {
         var jsonData = $.csv.toObjects(data);
 
         console.log(jsonData);
-
-        $.each(jsonData, function (index, value) {
-
-            $('#showCSV').append(
-
-                '<li class="list-group-item align-items-center">' + 
-                        
-                         
-                            value["公司統編"] +
-                         '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
-
-                         
-                            value["公司名稱"] +
-                            '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
-
-                          
-                            value["發票開立月份"] +
-                            '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
-                          
-                            value["發票開立張數"] +
-                         
-
+        $("#enter").click(function () { 
+            var filterData = $(jsonData).filter(function(index){
+                return jsonData[index].公司統編 == $("#number").val();
+            });
+            var totalfill = 0;
+            $(".fill").remove();
+            $.each(filterData, function (index, value) {
+                totalfill += parseInt(value["發票開立張數"]);
+                $('#showCSV').append(
+    
+                    '<li class="list-group-item align-items-center'+
+                        ' fill '+ value["公司統編"] + '">' + 
+                    value["公司統編"] +
+                    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+                             
+                    value["公司名稱"] +
+                    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+    
+                              
+                    value["發票開立月份"] +
+                    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+                              
+                    value["發票開立張數"] +
+                             
                     '</li>'
-            );
-
+                );
+    
+            });
+            if(filterData[1] !=null){
+            $('#showCSV').append('<li class="list-group-item align-items-center fill">'+'總開立發票張數 ' +totalfill +'</li>')}
         });
+        
 
     } // end: Ajax success API call
 
 }); // end: of Ajax call
 });
+
